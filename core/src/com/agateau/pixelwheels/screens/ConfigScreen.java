ea2988bcc2482d61b47edbd138348df5b46b02c8
Aurelien Gateau <mail@agateau.com>
2019-08-26 23:26:44 +0200

    ConfigScreen: Start creating an "About" tab

--- core/src/com/agateau/pixelwheels/screens/ConfigScreen.java
+++ core/src/com/agateau/pixelwheels/screens/ConfigScreen.java
@@ -22,10 +22,12 @@ import com.agateau.pixelwheels.Constants;
 import com.agateau.pixelwheels.GameConfig;
 import com.agateau.pixelwheels.PwGame;
 import com.agateau.pixelwheels.PwRefreshHelper;
+import com.agateau.pixelwheels.VersionInfo;
 import com.agateau.pixelwheels.gameinput.GameInputHandlerFactories;
 import com.agateau.pixelwheels.gameinput.GameInputHandlerFactory;
 import com.agateau.pixelwheels.gameinput.GamepadInputHandler;
 import com.agateau.pixelwheels.gameinput.KeyboardInputHandler;
+import com.agateau.pixelwheels.utils.StringUtils;
 import com.agateau.ui.UiAssets;
 import com.agateau.ui.UiBuilder;
 import com.agateau.ui.anchor.AnchorGroup;
@@ -127,6 +129,11 @@ public class ConfigScreen extends PwStageScreen {
                 });
                 group.addItemWithLabel("Fullscreen:", fullscreenSwitch);
             }
+        }
+
+        {
+            MenuItemGroup group = tab.addPage("About");
+            group.addLabel(StringUtils.format("Pixel Wheels %s", VersionInfo.VERSION));
         }
 
         {
